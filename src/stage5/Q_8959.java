2df2810c4cb22506d32e3a2cac066a5e93e42806
dev-Ji <wendyyi@naver.com>
2021-05-02 00:12:29 +0900

    2021.05.02

--- src/stage5/Q_8959.java
+++ src/stage5/Q_8959.java
@@ -3,7 +3,7 @@ package stage5;
 // "OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 
 // 예를 들어, 10번 문제의 점수는 3이 된다.
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 // "OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
@@ -67,7 +67,5 @@ public class Q_8959 {
 
             System.out.println(score);
         }
-
-        
     }
 }
